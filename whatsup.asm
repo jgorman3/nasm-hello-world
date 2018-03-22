d784019556f0aec36b02da5fcf0e6d6eb4ac7b2d
Joe Gorman <jgorman3@lion.lmu.edu>
2018-03-21 23:01:07 -0700

    added comments, easier to read

--- whatsup.asm
+++ whatsup.asm
@@ -1,15 +1,15 @@
-        global        start
-        section        .text
+        global     start
+        section    .text
 start:
-        mov        rax,0x2000004
-        mov        rdi,1
-        mov        rsi,phrase
-        mov        rdx,17
-        syscall
+        mov        rax,    0x2000004    ;system call for write
+        mov        rdi,    1
+        mov        rsi,    phrase       ;move address of string to output
+        mov        rdx,    17           ;number of bytes in string
+        syscall                         ;invoke OS to write
 
-        mov        rax,0x2000001
-        mov        rdi,0
-        syscall    
+        mov        rax,    0x2000001    ;system call for exit
+        mov        rdi,    0            ;exit code is 0
+        syscall                         ;invoke OS to exit
     
-        section        .data
-phrase: db        ‘最近怎么样？’,10
+        section    .data
+phrase: db         ‘最近怎么样？’,10      ;message loaded in data section
